c362d5fb32eb9240f66122742c99bbf204869da8
reveng-e <riddhishah4141@gmail.com>
2025-07-14 16:26:20 -0400

    db post and delete

--- closet_organizer/closet/views.py
+++ closet_organizer/closet/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import ClothingItem
 import uuid
+from collections import defaultdict
+import json
 
 def index(request):
     if 'user_id' not in request.session:
@@ -10,22 +12,35 @@ def index(request):
     user_id = request.session['user_id']
 
     if request.method == 'POST':
-        name = request.POST.get('itemName')
-        category = request.POST.get('itemCategory')
-        description = request.POST.get('itemDescription')
-        image = request.FILES.get('imageInput')
+        type = request.POST.get('type')
+        if type == 'additem':
+            name = request.POST.get('itemName')
+            category = request.POST.get('itemCategory')
+            description = request.POST.get('itemDescription')
+            image = request.FILES.get('imageInput')
 
-        try:
-            ClothingItem.objects.create(
-                name=name,
-                category=category,
-                description=description,
-                image=image,
-                user_id=user_id
-            )
-            return JsonResponse({'success': True})
-        except Exception as e:
-            return JsonResponse({'success': False, 'error': str(e)})
-
+            try:
+                ClothingItem.objects.create(
+                    name=name,
+                    category=category,
+                    description=description,
+                    image=image,
+                    user_id=user_id
+                )
+            except Exception as e:
+                return JsonResponse({'success': False, 'error': str(e)})
+        elif type == 'deleteitem':
+            try:
+                item_id = request.POST.get('item_id')
+                item = ClothingItem.objects.get(id=item_id, user_id=user_id)
+                item.delete()
+            except (ClothingItem.DoesNotExist, json.JSONDecodeError):
+                return JsonResponse({'success': False, 'error': 'Invalid request'}, status=400)
+    
+    
     items = ClothingItem.objects.filter(user_id=user_id)
-    return render(request, 'index.html', {'items': items})
+    categorized_items = defaultdict(list)
+    for item in items:
+        categorized_items[item.category].append(item)
+    
+    return render(request, 'index.html', {'categorized_items': dict(categorized_items)})
--- closet_organizer/utils.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-connection_string = 'mongodb+srv://riddhi:<Jaden*0409>@closet.mk9xkpg.mongodb.net/?retryWrites=true&w=majority&appName=Closet'
-client = MongoClient(connection_string)
-db = client['closet'] 
-
